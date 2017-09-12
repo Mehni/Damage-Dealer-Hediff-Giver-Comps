58edc43d0e428b9b990772093177f89193b554db
fyarn <1361631+fyarn@users.noreply.github.com>
2017-09-12 16:46:02 -0700

    - added hediffgiver comp and hediffgiver properties - started mod structure to test

---
+++ Source/HeDiffGiver/CompProperties_HeDiffGiver.cs
@@ -0,0 +1,18 @@
+﻿using Verse;
+
+namespace HediffGiver
+{
+    public class CompProperties_HediffGiver : CompProperties
+    {
+        public int damageAmount;
+        public DamageDef damageDef;
+        public bool affectInteractionCell;
+        public bool affectAdjacentCells;
+        public bool damagesSelf;
+        public int ticksBetweenDamage;
+
+        public CompProperties_HediffGiver() {
+			compClass = typeof(CompHediffGiver);
+        }
+    }
+}
